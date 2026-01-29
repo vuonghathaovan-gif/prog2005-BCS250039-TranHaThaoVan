f379848acc0c71ba2e142a63e5711f17bbb1c14f
vuonghathaovan-gif <vuonghathaovan@gmail.com>
2026-01-29 13:35:20 +0700

    Update 05.ExFive.py

--- Chapter 01/05.ExFive.py
+++ Chapter 01/05.ExFive.py
@@ -15,14 +15,12 @@ print("Điểm trung bình:", average_score)
 print("Kiểu dữ liệu của average_score:", type(average_score))
 print()
 
-# Tạo biến mới từ các biến đã có
+# Tạo biến 
 age_next_year = age + 1
 doubled_score = average_score * 2
 
-# In ra màn hình
 print("Tuổi năm sau:", age_next_year)
 print("Kiểu dữ liệu của age_next_year:", type(age_next_year))
 print()
-
 print("Điểm nhân đôi:", doubled_score)
 print("Kiểu dữ liệu của doubled_score:", type(doubled_score))
