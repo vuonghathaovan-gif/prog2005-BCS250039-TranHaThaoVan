57ccae8e2506a0e3fcaa1c55d0000fe0f7d378b6
vuonghathaovan-gif <vuonghathaovan@gmail.com>
2026-01-29 13:32:57 +0700

    Update 01.ExOne.py

--- Chapter 01/01.ExOne.py
+++ Chapter 01/01.ExOne.py
@@ -1,10 +1,5 @@
-# Tạo biến số nguyên (integer)
 so_nguyen = 250039
-
-# Tạo biến số thực (float)
 so_thuc = 25.0039
-
-# Tạo biến chuỗi (string)
 chuoi = "TranHaThaoVan BCS250039"
 
 # In ra các biến và kiểu dữ liệu của chúng
@@ -17,4 +12,5 @@ print("Kiểu dữ liệu: Số thực ", type(so_thuc))
 print()
 
 print("Chuỗi:", chuoi)
+
 print("Kiểu dữ liệu:Chuỗi ", type(chuoi))
