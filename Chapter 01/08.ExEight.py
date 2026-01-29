0eae15ba27a8b224dcaafd94cfb070f7729bbe3b
vuonghathaovan-gif <vuonghathaovan@gmail.com>
2026-01-29 13:36:08 +0700

    Update 08.ExEight.py

--- Chapter 01/08.ExEight.py
+++ Chapter 01/08.ExEight.py
@@ -1,11 +1,10 @@
 # Định nghĩa hàm greet với đối số mặc định
 def greet(name="Student"):
     print("Hello,", name + "!")
-
 ten = input("Nhập tên của bạn (Enter để bỏ qua): ")
-
 if ten:
     greet(ten)
 else:
     greet()
+
 
