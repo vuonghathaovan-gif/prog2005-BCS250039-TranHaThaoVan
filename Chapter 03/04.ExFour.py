33b2f32fbe682259cbda9521ef5406fdf25c49df
vuonghathaovan-gif <vuonghathaovan@gmail.com>
2026-03-05 12:56:47 +0700

    Create 04.ExFour.py

---
+++ Chapter 03/04.ExFour.py
@@ -0,0 +1,7 @@
+so_nguyen = [5, 2, 9, 1, 5, 3]
+so_nguyen.sort()
+print("Sau khi sắp xếp tăng dần:", so_nguyen)
+so_nguyen.reverse()
+print("Sau khi đảo ngược:", so_nguyen)
+soi = so_nguyen.count(5)
+print(f"Số 5 xuất hiện {soi} lần.")
