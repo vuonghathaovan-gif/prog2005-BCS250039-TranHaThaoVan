4c4428ec25ace697b42d9d95953d4715da3dcdfd
vuonghathaovan-gif <vuonghathaovan@gmail.com>
2026-03-05 12:48:52 +0700

    Create 02.ExTwo.py

---
+++ Chapter 03/02.ExTwo.py
@@ -0,0 +1,6 @@
+ban_be = ["Ly", "Tâm", "Hà"]
+print("Danh sách ban đầu:", ban_be)
+ban_be.append("Tuệ Minh")
+print("Sau khi thêm bạn mới:", ban_be)
+ban_be.pop(1)
+print("Sau khi xóa bạn thứ 2:", ban_be)
