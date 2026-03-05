2cb06c875c88788f6f7533f4bdb181eb8dc5cad5
vuonghathaovan-gif <vuonghathaovan@gmail.com>
2026-03-05 12:46:32 +0700

    Create 01.ExOne.py

---
+++ Chapter 03/01.ExOne.py
@@ -0,0 +1,5 @@
+mon_hoc = ["Cơ sở lập trình", "Đại số tuyến tính", "Lịch sử đảng", "Triết học mác-lênin"]
+print(f"Số lượng môn học: {len(mon_hoc)}")
+print("Danh sách môn học:")
+for mon in mon_hoc:
+    print(mon)
